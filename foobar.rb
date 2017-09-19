6c6b9cb789cd4b3c2fe1c6ccf6ec6a5a5d2c5e5e
mtrinh11@gmail.com <mtrinh11@gmail.com>
2017-09-19 13:08:07 -0700

    1st submit

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,28 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    result = Array.new
+    a.each_with_index { |val, index| a[index] = a[index].to_i}
+    a.each_with_index { |val, index| a[index] +=2}
+    a.each_with_index { |val, index| result << val if val % 2 == 0 and val < 10}
+    sum = 0
+    result.each { |x| sum += x}
+    sum
+  # res = a
+  # size = a.length 
+  # ind = 0
+
+  # while size > 0 do
+  # 	res[ind] = res[ind].to_i
+  # 	res[ind] += 2 
+  # 	size -=1
+  # 	ind+=1
+  # end
+  
+  # finay = Array.new(a.length)
+  # while 
+
+
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,28 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  x = a + b
+  y  = x**2
+  y
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  result = a.sort
+  size = a.length 
+  ind = 0
+  while size > 0 do
+  	result[ind] += 1
+  	size -=1
+  	ind+=1
+  end
+  result
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+ result = first_name + " " + last_name
+ result
 end
 
 def blockin_time(a)
